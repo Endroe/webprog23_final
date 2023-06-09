17013c28a5c51ceed0f80f1d9ddd6f900f198fa4
boringjawid <atss2080@gmail.com>
2023-06-09 17:09:06 +0200

    tiny changes to shop.php

--- final/shop.php
+++ final/shop.php
@@ -1,4 +1,5 @@
 <?php include 'tpl/head.php'; ?>
+    
     <h1>Emoji Shop</h1>
 
     <h2>Available Emojis</h2>
@@ -8,9 +9,6 @@
         <li>❤️ <span class="price">WINS 78</span> <button class="buy-button">Buy</button></li>
         <!-- Add more emoji items -->
     </ul>
-
-    <h2>Enter your username</h2>
-    <input type="text" id="username-input" placeholder="Username">
 
     <!-- <script src="shop.js"></script> For checking how many wins a player has and what the player can purchase; once purchase done, substract the amount of wins from players total wins.-->
 </body>
