05de8d43aa0d6c09cd15020bd93c962ede12ed80
Endroe <andrewdavidgeddes@gmail.com>
2023-06-15 00:08:49 +0200

    updated the front page a bit

--- index.php
+++ index.php
@@ -1,30 +1,18 @@
 <?php include 'tpl/head.php'; ?>
-<div class="container">
-    <h1 class="text-center my-4">Tic Tac Toe</h1>
-    <div class="row justify-content-center">
-        <!-- Lobby Creation -->
-        <div class="col-md-6">
+    <main role="main" class="inner cover">
+        <h1>Tic Tac Toe.</h1>
+        <p class="lead">Players can test their skills against opponents and strive to achieve the top positions on the leaderboard. Emojis can be added to accounts to add a touch of fun and visual appeal.</p>
+        <p class="lead">
+            <a href="play.php" class="btn btn-lg btn-primary w-100">Play now!</a>
+        </p>
+        <h1>Leaderboard</h1>
+        <div id="leaderboard"></div>
+    </main>
 
-        </div>
-    </div>
-    <hr />
-    <div class="justify-content-center mt-4">
-        <h4 class="d-flex justify-content-between align-items-center mb-3">
-            <span class="text-primary">Highscores</span>
-        </h4>
-
-        <div id="leaderboard">
-
-        </div>
-        <div class="justify-content-center mt-4">
-            <a href="play.php">
-                <button onclick='updateLeaderboard("Andrew", "win")' class="btn btn-success w-100">Try out the game for yourself!</button>
-            </a>
-        </div>
-    </div>
+    <footer class="mastfoot mt-auto">
+        <p>Created by Andrew, Jawid & Cody</p>
+    </footer>
 </div>
 </body>
 
 </html>
-<script>
-</script>
