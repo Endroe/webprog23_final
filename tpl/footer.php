b263ad6d97e584a7c4219e82d760e18d8e651d73
Endroe <andrewdavidgeddes@gmail.com>
2023-06-16 18:27:08 +0200

    footer added

---
+++ tpl/footer.php
@@ -0,0 +1,16 @@
+    <footer class="mastfoot mt-auto">
+        <p>Created by Andrew, Jawid & Cody</p>
+    </footer>
+</div>
+</body>
+
+<!-- Scripts -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js" integrity="sha512-bLT0Qm9VnAYZDflyKcBaQ2gg0hSYNQrJ8RilYldYQ1FxQYoCLtUjuuRuZo+fjqhx/qtq/1itJ0C2ejDxltZVFg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/4.6.1/js/bootstrap.min.js" integrity="sha512-UR25UO94eTnCVwjbXozyeVd6ZqpaAE9naiEUBK/A+QDbfSTQFhPGj5lOR6d8tsgbBk84Ggb5A3EkjsOgPRPcKA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script src="https://cdn.jsdelivr.net/npm/js-cookie@3.0.5/dist/js.cookie.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/animejs/3.2.1/anime.min.js"></script>
+
+<script type="application/javascript" src="js/main.js"></script>
+<script type="application/javascript" src="js/highscores.js"></script>
+<script type="application/javascript" src="js/game.js"></script>
+</html>
