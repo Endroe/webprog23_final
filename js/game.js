8ac73fe98acc4d70ef10d8f4804373bd989245cc
boringjawid <atss2080@gmail.com>
2023-06-16 18:37:23 +0200

    Checked for correct indentation, added docstrings and comments.

--- js/game.js
+++ js/game.js
@@ -1,35 +1,48 @@
+/**
+ * Tic Tac Toe Game
+ */
 
+// Constants
 const player1 = 'X';
 const player2 = 'O';
+
+// Game grids
 let grid = [
   ['', '', ''],
   ['', '', ''],
   ['', '', '']
 ];
-
-let localGrid = [ // This is the grid that gets displayed on screen.
+let localGrid = [
   ['', '', ''],
   ['', '', ''],
   ['', '', '']
 ];
 
+// Player information
 let lockedUsername = null;
 let username1 = null;
 let username2 = null;
+
+// Game state
 let gameStringState = "notInGame";
 
+// Lobby information
 let lobbyKey = Cookies.get('lobby');
 let lastUpdated = null; // Variable to track the last updated timestamp
 
-// Polling interval in milliseconds (e.g., update every 2 seconds)
+// Polling interval in milliseconds (e.g., update every second)
 const pollingInterval = 1000;
 
-// Function to start the polling for live updates https://levelup.gitconnected.com/polling-in-javascript-ab2d6378705a?gi=f09e207a63ec
+/**
+ * Start the polling for live updates
+ * @link https://levelup.gitconnected.com/polling-in-javascript-ab2d6378705a?gi=f09e207a63ec
+ */
 function startPolling() {
   setInterval(function() {
     retrieveGameData();
     checkForUpdates();
     updatePage();
+
     if (gameStringState === "player1win" || gameStringState === "player2win") {
       handleWinReceive();
     }
@@ -39,7 +52,10 @@ function startPolling() {
   }, pollingInterval);
 }
 
-// Function to update the game board with the retrieved data
+/**
+ * Update the game board with the retrieved data
+ * @param {Array} gameData - The retrieved game data
+ */
 function updateGameBoard(gameData) {
   let board = gameData;
   for (let row = 0; row < 3; row++) {
@@ -53,56 +69,72 @@ function updateGameBoard(gameData) {
   }
 }
 
+/**
+ * Check if a move is legal
+ * @param {number} row - The row index of the move
+ * @param {number} col - The column index of the move
+ * @returns {boolean} - True if the move is legal, false otherwise
+ */
 function checkMoveLegality(row, col) {
   if (grid[row][col] !== "") {
     return false;
   }
   if (gameStringState === "player1turn" && username1 === lockedUsername) {
     return true;
+  } else {
+    return gameStringState === "player2turn" && username2 === lockedUsername;
   }
-  else return gameStringState === "player2turn" && username2 === lockedUsername;
 }
 
+/**
+ * Make a move on the game board
+ * @param {number} row - The row index of the move
+ * @param {number} col - The column index of the move
+ */
 function Move(row, col) {
-  
   let currentPlayer = null;
+
   if (checkMoveLegality(row, col) === false) {
     return;
   }
+
   if (gameStringState === "player1turn") {
     currentPlayer = player1;
   } else {
     currentPlayer = player2;
   }
+
   grid[row][col] = currentPlayer;
   document.getElementsByClassName('cell')[row * 3 + col].innerText = currentPlayer;
-  
+
   // Animate the cell
   var cellId = 'cell-' + row + '-' + col;
   var cell = document.getElementById(cellId);
   anime({
     targets: cell,
     rotateY: '360deg',
-      scale: [1, 1.2, 1],
-      easing: 'easeInOutSine',
-      duration: 1000,
-      complete: function() {
-          cell.style.backgroundColor = '#007bff';
-      }
+    scale: [1, 1.2, 1],
+    easing: 'easeInOutSine',
+    duration: 1000,
+    complete: function() {
+      cell.style.backgroundColor = '#007bff';
+    }
   });
+
   if (gameStringState === "player1turn") {
     gameStringState = "player2turn";
-  }
-  else if (gameStringState === "player2turn") {
+  } else if (gameStringState === "player2turn") {
     gameStringState = "player1turn";
-  }
-  else {
+  } else {
     gameStringState = "broken";
   }
+
   storeGameData(grid);
 }
 
-// Function to store game data in JSON file
+/**
+ * Store game data in JSON file
+ */
 function storeGameData() {
   const filename = lobbyKey + '.json';
   $.ajax({
@@ -125,10 +157,13 @@ function storeGameData() {
   });
 }
 
-// Function to retrieve game data from JSON file
+/**
+ * Retrieve game data from JSON file
+ * @returns {Promise} - A promise that resolves with the retrieved game data
+ */
 function retrieveGameData() {
   const filename = lobbyKey + '.json';
-  return $.ajax({ // We return this for when/then behaviours. Change this and it breaks!
+  return $.ajax({
     type: 'GET',
     url: 'games/retrieve_game_data.php',
     data: {
@@ -154,29 +189,30 @@ function retrieveGameData() {
   });
 }
 
-// Function to check for new updates since the last updated timestamp
+/**
+ * Check for new updates since the last updated timestamp
+ */
 function checkForUpdates() {
-  //const filename = lobbyKey + '.json';
   updateGameBoard(grid);
   lastUpdated = new Date().getTime(); // Update the last updated timestamp
   checkGameEnd();
 }
 
+/**
+ * Check if the game has ended
+ */
 function checkGameEnd() {
   if (checkWin("X")) {
     gameStringState = "player1win";
     storeGameData(grid);
     sendOwnResult("X");
     resetGame();
-  }
-  else if (checkWin("O")) {
+  } else if (checkWin("O")) {
     gameStringState = "player2win";
     storeGameData(grid);
     sendOwnResult("O");
     resetGame();
-  }
-  else if (checkTie()) {
-    //alert("It's a tie!");
+  } else if (checkTie()) {
     gameStringState = "gameTie";
     storeGameData(grid);
     sendOwnResult("tie");
@@ -184,49 +220,60 @@ function checkGameEnd() {
   }
 }
 
+/**
+ * Handle the win received from the server
+ */
 function handleWinReceive() {
   if (gameStringState === "player1win" && lockedUsername === username2) {
-    //sendOwnResult("X");
     gameStringState = "player2turn";
     resetGame();
-  }
-  else if (gameStringState === "player2win" && lockedUsername === username1) {
+  } else if (gameStringState === "player2win" && lockedUsername === username1) {
     gameStringState = "player1turn";
-    //sendOwnResult("O");
     resetGame();
-  } else if (gameStringState === "player1forfeit") {
-
   }
   storeGameData(grid);
 }
 
+/**
+ * Send the result of the game for the current player
+ * @param {string} winner - The winner of the game ("X" or "O") or "tie"
+ */
 function sendOwnResult(winner) {
-  if (player1 === winner && lockedUsername === username1) { // Client is X and X wins.
+  if (player1 === winner && lockedUsername === username1) {
     updateLeaderboard(lockedUsername, "win");
-  }
-  else if (player2 === winner && lockedUsername === username2) { // Client is O and O wins.
+  } else if (player2 === winner && lockedUsername === username2) {
     updateLeaderboard(lockedUsername, "win");
-  }
-  else if (winner === "tie") {
+  } else if (winner === "tie") {
     updateLeaderboard(lockedUsername, "tie");
-  }
-  else {
+  } else {
     updateLeaderboard(lockedUsername, "loss");
   }
 }
 
-
+/**
+ * Check if a player has won the game
+ * @param {string} player - The player to check ("X" or "O")
+ * @returns {boolean} - True if the player has won, false otherwise
+ */
 function checkWin(player) {
   // Check rows
   for (let i = 0; i < 3; i++) {
-    if (grid[i][0] === player && grid[i][1] === player && grid[i][2] === player) {
+    if (
+      grid[i][0] === player &&
+      grid[i][1] === player &&
+      grid[i][2] === player
+    ) {
       return true;
     }
   }
 
   // Check columns
   for (let i = 0; i < 3; i++) {
-    if (grid[0][i] === player && grid[1][i] === player && grid[2][i] === player) {
+    if (
+      grid[0][i] === player &&
+      grid[1][i] === player &&
+      grid[2][i] === player
+    ) {
       return true;
     }
   }
@@ -242,11 +289,14 @@ function checkWin(player) {
   return false;
 }
 
-
+/**
+ * Check if the game is a tie
+ * @returns {boolean} - True if the game is a tie, false otherwise
+ */
 function checkTie() {
   for (let row = 0; row < 3; row++) {
     for (let col = 0; col < 3; col++) {
-      if (grid[row][col] === '') {
+      if (grid[row][col] === "") {
         return false;
       }
     }
@@ -254,23 +304,28 @@ function checkTie() {
   return true;
 }
 
-
+/**
+ * Reset the game board and state
+ */
 function resetGame() {
-    grid.forEach(row => row.fill(''));
-    const cells = document.getElementsByClassName('cell');
-    for (let i = 0; i < cells.length; i++) {
-      cells[i].innerText = '';
-      cells[i].style.backgroundColor = 'aliceblue';
-    }
-    storeGameData(grid)
+  grid.forEach(row => row.fill(""));
+  const cells = document.getElementsByClassName("cell");
+  for (let i = 0; i < cells.length; i++) {
+    cells[i].innerText = "";
+    cells[i].style.backgroundColor = "aliceblue";
+  }
+  storeGameData(grid);
 }
 
+/**
+ * Handle a player leaving the game
+ * @param {string} lockedUsername - The username of the player who left
+ */
 function handlePlayerLeave(lockedUsername) {
   if (lockedUsername === username1) {
     updateLeaderboard(lockedUsername, "loss");
     gameStringState = "player1forfeit";
-  }
-  else if (lockedUsername === username2) {
+  } else if (lockedUsername === username2) {
     updateLeaderboard(lockedUsername, "loss");
     gameStringState = "player2forfeit";
   }
@@ -278,11 +333,13 @@ function handlePlayerLeave(lockedUsername) {
   resetGame();
 }
 
+/**
+ * Handle a forfeit received from the server
+ */
 function onReceiveForfeit() {
   if (gameStringState === "player1forfeit") {
     gameStringState = "player2turn";
-  }
-  else if (gameStringState === "player2forfeit") {
+  } else if (gameStringState === "player2forfeit") {
     gameStringState = "player1turn";
   }
   storeGameData(grid);
@@ -299,47 +356,52 @@ $(document).ready(function() {
   $('#create-lobby-btn').click(function() {
     createLobby();
   });
-  
+
   $('#generate-lobby-key-btn').click(function() {
     generateRandomKey();
     createLobby();
   });
-  
+
+  /**
+   * Create a lobby and initialize the game
+   */
   function createLobby() {
     const lobby = $('#lobby-key-input').val();
-  
+
     // Hide helpers.
     $('#alert-invalidlobby').hide();
     $('#alert-roomNotAvailable').hide();
     $('#alert-duplicatePlayer').hide();
-  
+
     // Check if logged in.
     if (Cookies.get('username') === undefined) {
       $('#alert-notloggedin').show();
       return;
     }
-  
+
     // Validate Lobby Code
     if (validateLobby(lobby) === false) {
       $('#alert-invalidlobby').show();
       return;
     }
-  
+
     Cookies.set('lobby', lobby);
     Cookies.set('player', '');
     lobbyKey = lobby;
-  
+
     $('#game-board').show();
     initializeGame();
   }
-  
+
+  /**
+   * Generate a random lobby key and set it in the input field
+   */
   function generateRandomKey() {
     const min = 100000;
     const max = 999999;
     const randomKey = Math.floor(Math.random() * (max - min + 1)) + min;
     $('#lobby-key-input').val(randomKey);
   }
-  
 
   // Calls the leavingPlayer function
   $('#leave-game-btn').click(function() {
@@ -347,19 +409,25 @@ $(document).ready(function() {
     if (gameStringState === "player1turn" && lockedUsername === username1) {
       console.log("test1");
       handlePlayerLeave(lockedUsername);
-    }
-    else if (gameStringState === "player2turn" && lockedUsername === username2) {
+    } else if (gameStringState === "player2turn" && lockedUsername === username2) {
       handlePlayerLeave(lockedUsername);
     }
   });
 });
 
+/**
+ * Validate the lobby code
+ * @param {string} lobbyInput - The lobby code to validate
+ * @returns {boolean} - True if the lobby code is valid, false otherwise
+ */
 function validateLobby(lobbyInput) {
   const re_lobby = /^[a-z0-9_-]{6}$/igm;
   return re_lobby.test(lobbyInput) !== false;
 }
 
-// Function to initialize the game
+/**
+ * Initialize the game
+ */
 function initializeGame() {
   lastUpdated = new Date().getTime(); // Update the last updated timestamp
   lockedUsername = Cookies.get('username');
@@ -388,11 +456,11 @@ function initializeGame() {
     if (username1 == null) {
       username1 = lockedUsername;
       gameStringState = "waiting";
-      storeGameData(grid)
+      storeGameData(grid);
     } else if (username2 === "") {
       username2 = lockedUsername;
       gameStringState = "player1turn";
-      storeGameData(grid)
+      storeGameData(grid);
     } else {
       $('#alert-roomNotAvailable').show();
       username1 = null;
@@ -419,6 +487,9 @@ function initializeGame() {
   });
 }
 
+/**
+ * Update the game page elements
+ */
 function updatePage() {
   // Handle the status indicator.
   switch (gameStringState) {
@@ -457,9 +528,7 @@ function updatePage() {
         $('#player2title').text(username2);
         $('#player2emoji').text(user2.oIcon);
         fetchAndDisplayUsersStats(username1, '#user1StatsDiv', username2, '#user2StatsDiv');
-
-      }
-      else {
+      } else {
         $('#player1title').text(username2);
         $('#player1emoji').text(user2.oIcon);
         $('#player2title').text(username1);
@@ -469,13 +538,32 @@ function updatePage() {
     });
   });
 }
+/**
+ * Fetch user statistics and display them
+ * @param {string} username1 - The first username
+ * @param {string} divId1 - The ID of the first div to display statistics
+ * @param {string} username2 - The second username
+ * @param {string} divId2 - The ID of the second div to display statistics
+ */
 function fetchAndDisplayUsersStats(username1, divId1, username2, divId2) {
-  // Function to calculate the win rate percentage
+  /**
+   * Calculate the win rate percentage
+   * @param {number} count - The number of wins
+   * @param {number} total - The total number of games played
+   * @returns {number} - The win rate percentage
+   */
   function calculatePercentage(count, total) {
     return Math.round((count / total) * 100);
   }
 
-  // Function to generate the HTML for the stats list item
+  /**
+   * Generate the HTML for the stats list item
+   * @param {string} title - The title of the stats
+   * @param {number} percentage - The win rate percentage
+   * @param {number} count - The count of wins, losses, or ties
+   * @param {string} colorClass - The CSS class for the item color
+   * @returns {string} - The HTML for the stats list item
+   */
   function generateStatsListItem(title, percentage, count, colorClass) {
     return `
       <li class="list-group-item d-flex justify-content-between bg-body-tertiary">
@@ -491,6 +579,7 @@ function fetchAndDisplayUsersStats(username1, divId1, username2, divId2) {
   // Fetch user stats using AJAX or fetch API
   $.getJSON('scores/highscores.json', function(data) {
     let user1Stats, user2Stats;
+
     // Find the stats for the specified usernames
     data.forEach(function(stats) {
       if (stats.username === username1) {
@@ -539,4 +628,3 @@ function fetchAndDisplayUsersStats(username1, divId1, username2, divId2) {
     $(divId2).html(user2StatsHTML);
   });
 }
-
